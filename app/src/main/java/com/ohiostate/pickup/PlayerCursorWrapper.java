5e91fd2115b21788f74b015370ba70dfd998c28c
BlakeHoward <BlakeHoward@users.noreply.github.com>
2016-11-30 15:44:20 -0500

    Create PlayerCursorWrapper.java

---
+++ app/src/main/java/com/ohiostate/pickup/PlayerCursorWrapper.java
@@ -0,0 +1,45 @@
+package com.ohiostate.pickup;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+
+import com.facebook.Profile;
+import com.ohiostate.pickup.DatabaseSchema.PlayerTable;
+
+/**
+ * Created by blakehoward on 11/23/16.
+ */
+
+public class PlayerCursorWrapper extends CursorWrapper {
+    // Constructor
+    public PlayerCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+    public Player getPlayer(){
+
+        int playerID = getInt(getColumnIndex(PlayerTable.Cols.ID));
+        String playerFirstName = getString(getColumnIndex(PlayerTable.Cols.FIRST_NAME));
+        String playerLastName = getString(getColumnIndex(PlayerTable.Cols.LAST_NAME));
+        String playerEmail = getString(getColumnIndex(PlayerTable.Cols.EMAIL));
+        String playerGender = getString(getColumnIndex(PlayerTable.Cols.GENDER));
+
+        Player player;
+        ProfileFunctionality profileFunctionality = ProfileFunctionality.get(PickUp.getContext());
+
+        player = profileFunctionality.getPlayer(Long.parseLong(Profile.getCurrentProfile().getId()));
+        if (player != null) {
+
+            player.setId(playerID);
+            player.setFirst_name(playerFirstName);
+            player.setLast_name(playerLastName);
+            player.setEmail(playerEmail);
+            player.setGender(playerGender);
+        }
+        else {
+            player = new Player(Long.parseLong(Profile.getCurrentProfile().getId()));
+        }
+
+        return player;
+    }
+}
